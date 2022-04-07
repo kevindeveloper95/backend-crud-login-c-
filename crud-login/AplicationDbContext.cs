7a9245a4967de7940ed283c0f99262166b81ac3b
kevin meneses parga <kevindeveloper95@hotmail.com>
2022-04-07 07:17:04 -0500

    faltaron algunas funcionaldades sin resolver como pasar la aplicacion por insomnia o postman

---
+++ crud-login/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using crud_login.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_login
+    
+{
+    public class AplicationDbContext: DbContext
+    {
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
---
+++ crud-login/Model/TarjetaCredito.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crud_login.Model
+{
+    public class TarjetaCredito
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(100)")]
+        public string Titular { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string NumeroTarjeta { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string FechaExpedicion { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
+}
